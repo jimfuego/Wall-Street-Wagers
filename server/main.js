1a95e16dbca908d9fa261baf130f4e5b4a8f5f47
jimmyb <jimmyb89@yahoo.com>
2019-04-04 16:53:53 -0700

    minor changes/imports

--- server/main.js
+++ server/main.js
@@ -1,31 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
-
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
-}
+import "../imports/api/alphaVantage.js";
+import "../imports/api/achievements.js";
+import "../imports/api/bets.js";
+import "../imports/api/market.js";
+import "../imports/api/users.js";
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
-
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
 });
