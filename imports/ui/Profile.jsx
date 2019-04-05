a5d6989a9db7c6fe53475ea3e7d4dde3bf837f21
jimmyb <jimmyb89@yahoo.com>
2019-04-04 22:44:46 -0700

    late

--- imports/ui/Profile.jsx
+++ imports/ui/Profile.jsx
@@ -1,44 +1,51 @@
 import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
 import Achievements from "./Achievements.jsx";
 import PredictionStats from "./Prediction-stats.jsx";
 import Gambler from "./Gambler.jsx";
+import ButtonAppBar from "./ButtonAppBar.jsx";
+import Landing from "./Landing.jsx";
+import Registration from "./Registration.jsx";
+import Bet from "./Bet.jsx";
+import LoginTab from "./Login-tab.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { withTracker } from "meteor/react-meteor-data";
 import { Meteor } from "meteor/meteor";
+import { Redirect } from "react-router-dom";
 import {withRouter} from "react-router-dom";
 import Bio from "./Bio.jsx";
 import MenuBar from "./MenuBar.jsx";
-import ButtonAppBar from "./ButtonAppBar.jsx";
-import { Grid, Button } from '@material-ui/core';
+
+
 
 class Profile extends Component{
+
     constructor(props) {
     super(props);
   }
 
-  onClick(event) {
-    event.preventDefault();
-     this.props.history.push("/bet");
-   }
 render(){
 	return (
 		<div className="container-fluid">
-        <div className="row">
-          <div className="col s12 12"><MenuBar /></div>
-          {/*<div className=" col s12 m7"><Gambler /></div>*/}
-          <div className="col s12 m5"><PredictionStats /></div>
+          <div className="row">
+            <div className="col s12 12"><MenuBar /></div>
+            {/*<div className=" col s12 m7"><Gambler /></div>*/}
+            <div className="col s12 m5"><PredictionStats /></div>
+          </div>
+          <div className="row">
+           {/*<div className="col s12 m9"><Bio/></div>*/}
+           {/*<div className="achievements"><Achievements /></div>*/}
+          </div>
         </div>
-        <div>
-          <Button variant="outlined" color="primary" style={{ textTransform: "none" }} onClick={this.onClick} >Start Betting!</Button>
-        </div>
-          {/*<div className="col s12 m9"><Bio/></div>*/}
-          {/*<div className="achievements"><Achievements /></div>*/}
-    </div>
-  );
+          );
+      }
+
   }
-}
+
 
   export default withTracker (() => {
-  return {
+    return {
     user: Meteor.user()
-  }
+  };
 })(withRouter(Profile));
