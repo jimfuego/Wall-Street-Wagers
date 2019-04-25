32b3a45a4fbd96b1896e09eaa44fe3b93971aee0
jimmyb <jimmyb89@yahoo.com>
2019-04-25 00:58:39 -0700

    fixed merge conflicts

--- imports/api/bets.js
+++ imports/api/bets.js
@@ -5,8 +5,7 @@ import { check } from "meteor/check";
 export const Bets = new Mongo.Collection("bets");
 
 //process.env.API_KEY
-// const alpha = require('alphavantage')({ key: process.env.API_KEY });
-const alpha = require('alphavantage')({ key: "AD7JAZ51CFHTA74D" });
+const alpha = require('alphavantage')({ key: process.env.API_KEY });
 
 //publish
 if (Meteor.isServer) {
