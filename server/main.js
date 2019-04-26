0366312a79665ee0cee512a792ecf8d02b7bf037
jimmyb <jimmyb89@yahoo.com>
2019-04-25 19:01:09 -0700

    server cron-job

--- server/main.js
+++ server/main.js
@@ -7,6 +7,8 @@ import "../imports/api/users.js";
 import "../imports/api/minimongo.js";
 import "../imports/api/presences.js"
 import "../imports/api/wager.js"
+import { CronJob } from 'cron';
+
 
 import { DDPRateLimiter } from "meteor/ddp-rate-limiter";
 
@@ -26,12 +28,33 @@ if (Meteor.isServer) {
         name(name) {
             return LISTS_METHODS.includes(name);
         },
-
         // Rate limit per connection ID
         connectionId() { return true; }
     }, 5, 1000);
 }
 
 Meteor.startup(() => {
-
+    new CronJob({
+        cronTime: '* * * * * *',
+        // use this wrapper if you want to work with mongo:
+        onTick: Meteor.bindEnvironment(() => {
+            // stuff happens
+            const d = new Date();
+            console.log('At One Second:', d);
+            //eval single-player bets
+            Meteor.call("bets.evaluateAll", (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            //eval multi-player bets
+            Meteor.call("wager.evaluateAll", (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }),
+        start: true,
+        timeZone: 'America/Los_Angeles',
+    });
 });
